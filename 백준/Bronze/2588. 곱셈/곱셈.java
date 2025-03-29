60847be2a28692b75c7f8665d0f9e0f416fde9e4
Salgu <warsfac@gmail.com>
2025-03-30 02:00:33 +0900

    [Bronze III] Title: 곱셈, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int B1 = B%10;
+        int B2 = (B%100)/10;
+        int B3 = (B - (B1%100))/100;
+
+        System.out.println(A*B1);
+        System.out.println(A*B2);
+        System.out.println(A*B3);
+        System.out.println(A*B);
+    }
+}
