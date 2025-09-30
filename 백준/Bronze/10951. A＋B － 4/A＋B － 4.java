839e0db1bc9c4fc1edc25a1d1ff60e3ea2a6a759
Salgu <warsfac@gmail.com>
2025-09-30 15:07:38 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -3,57 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
-public class Main{
+public class Main {
     
-    /*public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)throws IOException{
 
-        // while(true){
-        //     try {
-        //         String str = sc.nextLine(); // Ctrl+Z 누르면 예외 발생
-
-        //         String[] array = str.split(" ");
-        //         System.out.println(Integer.parseInt(array[0]) + Integer.parseInt(array[1]));
-
-        //     } catch (NoSuchElementException e) {
-        //         break;
-        //     }
-        // }
-
-        while(sc.hasNext()){
-            String str = sc.nextLine();
-
-            String[] array = str.split(" ");
-            System.out.println(Integer.parseInt(array[0]) + Integer.parseInt(array[1]));
-        }
-        
-
-    }*/
-
-    public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            try {
-                while(true){
-                    String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        String str = "";
 
-                    if(str != null){
-                        String[] array = str.split(" ");
-                        int result = Integer.parseInt(array[0]) + Integer.parseInt(array[1]);
-    
-                        bw.write(result + "\n");
-                    }else{
-                        break;
-                    }
-                }
+        while((str = br.readLine()) != null){
+            int A = Integer.parseInt(str.split(" ")[0]);
+            int B = Integer.parseInt(str.split(" ")[1]);
+            sb.append(A + B).append("\n");
 
-                bw.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            
+        }
+        bw.write(sb + "");
+        bw.flush();
     }
 }
