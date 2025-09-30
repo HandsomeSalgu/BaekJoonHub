78109b7338fc3af8865b9ec78ca1016172172143
Salgu <warsfac@gmail.com>
2025-09-30 13:39:22 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -9,35 +9,30 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        //값 받아오기
         String StringN = br.readLine();
 
+        //총 가져야될 설탕
         int N = Integer.parseInt(StringN);
 
-        int three = 0;
-        int five = 0;
-        int result = 0;
+        //결과 값(해당 안되면 -1
+        int result = -1;
 
+        //5로 나눴을 때 딱 맞아떨어지면 최소값은 N/5
         if(N % 5 == 0){
-            result = N / 5;
-        }else{
-            while(N >= 3){
-                three++;
-                N = N - 3;
-                if(N % 5 == 0){
-                    five = N / 5;
-                    N = 0;
+            result = N /5;
+        }
+        //5로 바로 안떨어지면 현재 설탕의 키로수에 최대 5kg의 수의 반복
+        else{
+            int fiveNum = N /5;
+
+            for(int i = fiveNum; i>=0; i--){
+                if((N - (5*i))%3 == 0){
+                    result = ((N - (5*i))/3) + i;
                     break;
                 }
             }
-
-            if(N == 0){
-                result = three + five;
-            }else{
-                result = -1;
-            }
         }
-
-        
 
         bw.write(result + "");
         bw.flush();
