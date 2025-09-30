997109a4b80eda827391e79f77105981132b5037
Salgu <warsfac@gmail.com>
2025-09-30 14:45:02 +0900

    [Bronze V] Title: 구구단, Time: 136 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
 
-        int a = sc.nextInt();
-
-        for(int i = 1; i<=9; i++){
-            System.out.println(a +" * " + i + " = " + a*i);
+        for(int i = 1; i<= 9; i++){
+            bw.write(N + " * " + i + " = " + (N*i));
+            if(i != 9){
+                bw.write("\n");
+            }
         }
+
+        bw.flush();
     }
 }
