9f1e45662488aa3edf38fd1e6ccd50b4cb4deee7
Salgu <warsfac@gmail.com>
2026-01-21 14:52:49 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 236 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+      
+        int N = sc.nextInt();
+
+        for(int i = N; i>=1; i--){
+            for(int j = 0; j<i; j++){
+                System.out.print("*");
+            }
+            if(i != 0){
+                System.out.println();
+            }
+            
+        }
+    }
+}
