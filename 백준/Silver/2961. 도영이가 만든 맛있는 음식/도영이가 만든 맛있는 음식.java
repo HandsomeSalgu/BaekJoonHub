7506be2250489eb5d3dd2f72f26707df7444a424
Salgu <warsfac@gmail.com>
2026-02-05 17:54:28 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 160 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -9,9 +9,6 @@ public class Main {
 	//정답
 	static int result = 0;
 	
-	//방문 조회
-	static boolean[] visited;
-	
 	//재료의 개수
 	static int N;
 
@@ -26,10 +23,6 @@ public class Main {
 		sArr = new int[N];
 		bArr = new int[N];
 		
-		visited = new boolean[N];
-		
-		
-		
 		for(int i = 0; i<N; i++) {
 			//신맛, 곱
 			int S = sc.nextInt();
@@ -43,9 +36,7 @@ public class Main {
 		result = Math.abs(sArr[0] - bArr[0]);
 		
 		dfs(1,0,0);
-		
 
-		
 		System.out.println(result);
 	}
 	
@@ -55,26 +46,17 @@ public class Main {
 		int sum = 0;
 		
 		int diff1 = 0;
-		int diff2 = 0;
 		
 		for(int i = depth; i<N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				multiply = s * sArr[i];
-				sum = b + bArr[i];
-				
-				diff1 = Math.abs(s-b) + Math.abs(sArr[i] - bArr[i]);
-				diff2 = Math.abs(multiply - sum);
-				
-				result = Math.min(result, diff1);
-				
-				if(diff1 >= diff2) {
-					result = Math.min(result, diff2);
-					dfs(multiply, sum, depth+1);
-				}
-				visited[i] = false;
-			}
 			
+			multiply = s * sArr[i];
+			sum = b + bArr[i];
+			
+			diff1 = Math.abs(multiply-sum);
+			
+			result = Math.min(result, diff1);
+			dfs(multiply, sum, i+1);
+		
 		}
 	}
 	
